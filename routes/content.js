fdb6a03e7fb6fd5a86048a3a6f4e11992f5b1bca
jooy3410 <jooy3410@naver.com>
2022-03-28 07:00:05 +0000

    실시간 실제 남은 공간

--- routes/content.js
+++ routes/content.js
@@ -7,7 +7,7 @@ const CircularJSON = require('circular-json');
 const qs = require('qs');
 
 
-//실시간 남은 자리
+//실시간 사용자에 빌려주는 공간
 app.get('/real_time_remaining_seats', function (req, res){
   axios.get('http://3.36.211.38:3000/status/car/space/possible')
   .then(data =>{
@@ -15,6 +15,17 @@ app.get('/real_time_remaining_seats', function (req, res){
     console.log(data.data.park_usenumber)
     console.log(data.data.park_setting['now_place'])
     res.render('content/remaining_seats.ejs', {'data' : data} )
+  });
+});
+
+//실시간 실제 남은 자리
+app.get('/actual_parking_space', function (req, res){
+  axios.get('http://3.36.211.38:3000/status/car/space/now/all')
+  .then(data =>{
+    console.log(data.data)
+    console.log(data.data.park_setting['TOTAL_SPACE'])
+    console.log(data.data.park_setting['현재 전체 주차 대수'])
+    res.render('content/actual_parking_space.ejs', {'data' : data} )
   });
 });
 
